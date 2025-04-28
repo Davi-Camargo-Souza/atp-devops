678f6c45960c30de0f9b35d01381221b074a2ed8
Davi-Camargo-Souza <camargod86@gmail.com>
2025-04-28 17:39:59 -0300

    adicionado entidade usuario

--- CrudUsuarios/Domain/CrudUsuarios.Domain/Contexts/Usuarios/Entities/Usuario.cs
+++ CrudUsuarios/Domain/CrudUsuarios.Domain/Contexts/Usuarios/Entities/Usuario.cs
@@ -2,5 +2,19 @@
 
 public class Usuario
 {
-    
+    protected Usuario() { }
+    public Usuario(string nome, int idade, string email, string senha, string telefone)
+    {
+        Nome = nome;
+        Idade = idade;
+        Email = email;
+        Senha = senha;
+        Telefone = telefone;
+    }
+
+    public string Nome { get; private set; }
+    public int Idade { get; private set; }
+    public string Email { get; private set; }
+    public string Senha { get; private set; }
+    public string Telefone { get; private set; }
 }
